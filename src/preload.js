8c31bdfa2c27b7d1883da3afa5e9bf5a807e463b
loopsaaage <maskerprc@gmail.com>
2024-12-17 23:08:26 +0800

    finished todo

--- src/preload.js
+++
@@ -1,8 +0,0 @@
-
-    const { contextBridge, ipcRenderer } = require('electron');
-
-    contextBridge.exposeInMainWorld('api', {
-        updateConfig: (credentials) => ipcRenderer.send('update-config', credentials),
-        onUpdateSuccess: (callback) => ipcRenderer.on('update-config-success', callback),
-        onUpdateFailure: (callback) => ipcRenderer.on('update-config-failure', callback)
-    });
